4694e1f8efc88848cfc3f80d66db0b2fc45d6247
sprimage <timilehin1999@gmail.com>
2023-12-11 20:42:15 -0600

    build

--- lib/env.d.ts
+++ lib/env.d.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import { HttpProxyAgent } from "http-proxy-agent";
 export declare const PROVIDER: ethers.InfuraProvider;
-export declare const SIGNER: ethers.Wallet;
+export declare const SIGNER: ethers.Wallet | ethers.HDNodeWallet;
 export declare const PORT: string | number;
 export declare const HOST: string;
 export declare const AGENT: HttpProxyAgent<string>;
